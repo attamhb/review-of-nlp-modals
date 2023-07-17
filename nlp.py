d282fb12c5623f68bde873f5449e2022030dfc8c
theodcod <attahassani@hotmail.com>
2023-07-17 13:50:27 -0700

    gitignore file created

--- nlp.py
+++ nlp.py
@@ -1,10 +1,20 @@
 
 
-from torchtext.datasets import IMDB
-train_dataset = IMDB(split='train')
-test_dataset = IMDB(split='test')
+import pandas as pd
+import numpy as np
 
-print(train_dataset) 
+#https://www.kaggle.com/datasets/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews?select=IMDB+Dataset.csv
+df = pd.read_csv('./data/imdb_data.csv')
+
+df_review = df['review']
+df_sentiment = df['sentiment']
+
+string =  ""
+[string + char for char in df_review]
+print(string)
+print(''.join(sorted(set(df_review[0]))))
+
+#print(df_sentiment[0])
 
 
 
